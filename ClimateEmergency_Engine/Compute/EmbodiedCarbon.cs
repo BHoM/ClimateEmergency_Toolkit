5e507d31a400e0dfd84cba28798b3f1f1e61cc3f
Al Fisher <Al.Fisher@burohappold.com>
2019-09-07 11:40:11 +0100

    Adding initial Compute EmbodiedCarbon method

---
+++ ClimateEmergency_Engine/Compute/EmbodiedCarbon.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BH.oM.Base;
+using BH.oM.Structure.MaterialFragments;
+
+namespace BH.Engine.ClimateEmergency
+{
+    public static partial class Compute
+    {
+        /***************************************************/
+        /****   Public Methods                          ****/
+        /***************************************************/
+
+        public static double EmbodiedCarbon(BHoMObject obj, IMaterialFragment material, CustomObject embodiedCarbonData)
+        {
+            double volume, density, embodiedCarbon;
+
+            if (obj.CustomData.ContainsKey("Volume"))
+            {
+                volume = (double)obj.CustomData["Volume"];
+            }
+            else
+            { 
+                return 0;
+            }
+
+
+            density = material.Density;
+
+
+            if (embodiedCarbonData.CustomData.ContainsKey("EmbodiedCarbon"))
+            {
+                embodiedCarbon = (double)embodiedCarbonData.CustomData["EmbodiedCarbon"];
+            }
+            else
+            {
+                return 0;
+            }
+
+            return volume * density * embodiedCarbon;
+        }
+
+        /***************************************************/
+
+    }
+}
