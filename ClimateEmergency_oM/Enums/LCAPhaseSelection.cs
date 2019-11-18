7949c8f954f20c1b42bfa11ec843b963019d0a64
Michael Hoehn <michael.hoehn@burohappold.com>
2019-11-18 12:35:45 -0500

    Building LCA Object and Phase selection enum

---
+++ ClimateEmergency_oM/Enums/LCAPhaseSelection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.ClimateEmergency.Enums
+{
+    public enum LCAPhaseSelection
+    {
+        A1,
+        A2,
+        A3,
+        A4,
+        A5,
+        B1,
+        B2,
+        B3,
+        B4,
+        B5,
+        B6,
+        B7,
+        C1,
+        C2,
+        C3,
+        C4,
+        D
+    }
+}
