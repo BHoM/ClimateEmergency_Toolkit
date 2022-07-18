6887e158e58d3e77a200934cbdda478cee392c99
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:06 +0100

    Update Assembly Information

--- ClimateEmergency_oM/Properties/AssemblyInfo.cs
+++ ClimateEmergency_oM/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ClimateEmergency_oM")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/ClimateEmergency_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ClimateEmergency_oM")]
